69cbd5d18ca1a84d34aa017f061dfd5a230f84f0
floodslake <129939125+floodslake@users.noreply.github.com>
2025-03-18 19:53:31 +0700

    Update the_script.sh

--- the_script.sh
+++ the_script.sh
@@ -61,10 +61,12 @@ while read -r channel_info; do
 
       video_convert_to_vod_flg="$(jq --raw-output '.video.convert_to_vod_flg' <<<"${live_info}")";
       [[ "${video_convert_to_vod_flg}" == 'true' ]] && video_convert_to_vod_flg='';
+      
       if [[ -z "$live_scheduled_start_at" ]]; then
       	live_scheduled_start_at_second=$(date --date="9999-12-31" '+%s');
       else
       	live_scheduled_start_at_second=$(date --date="${live_scheduled_start_at}" '+%s');
+      fi;
 
       title="$(jq --raw-output '.title' <<<"${live_info}")";
 
