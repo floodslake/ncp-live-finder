cda303a2a801f965d02d62831cc781e9c80a0369
floodslake <129939125+floodslake@users.noreply.github.com>
2025-05-18 00:04:59 +0700

    Update the_script_fc.sh

--- the_script_fc.sh
+++ the_script_fc.sh
@@ -222,6 +222,7 @@ fanclubs["524"]="dGVuc2hpLW5hbm8uY29t" #nn
 fanclubs["576"]="aG9zaGluby1zdXBpa2EuY29t" #spk
 fanclubs["618"]="bmlnaHRtYXJlLXNhbG9uLmNvbQ==" #mr
 fanclubs["677"]="bXVuZWF0c3UtZmMuY29t" #nefson
+fanclubs["764"]="cmlydXJ1LW9mZmljaWFsLmNvbQ==" #rrr
 
 for key in "${!fanclubs[@]}"; do
   decoded_string=$(decode_base64 "${fanclubs[$key]}")
